9ba95277ebfc77fcf1002f6e2358a7cbab2845d0
Aditya <adityakumaraddy05@gmail.com>
2026-01-08 23:17:03 +0530

    Backend: product scraping + APIs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import mongoose from 'mongoose';
-import { ValidationPipe } from '@nestjs/common';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  });
 
-  console.log('Connected MongoDB database:', mongoose.connection.name);
-
-  app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }),
-);
-
+  await app.listen(4000);
+  console.log('Backend running on http://localhost:4000');
 }
-
 bootstrap();
